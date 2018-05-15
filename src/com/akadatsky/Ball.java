61045b0bb1d9590fd4817790e3e51e845dcbd707
Oleksiy Shcherbyna <lenyashe@gmail.com>
2018-05-15 22:38:04 +0300

    first commit

--- src/com/akadatsky/Ball.java
+++ src/com/akadatsky/Ball.java
@@ -7,11 +7,13 @@ public class Ball {
 
     private final GraphicsContext GC;
 
+    private String name;
     private double diameter;
     private double x;
     private double y;
     private double xSpeed;
     private double ySpeed;
+
 
     public Ball(GraphicsContext gc) {
         this.GC = gc;
@@ -21,6 +23,46 @@ public class Ball {
 
         xSpeed = 2;
         ySpeed = 1.5;
+
+    }
+
+
+    public Ball(double diameter, double x, double y, double xSpeed, double ySpeed, GraphicsContext gc) {
+        this.diameter = diameter;
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+        this.GC = gc;
+    }
+
+
+    public GraphicsContext getGC() {
+        return GC;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getxSpeed() {
+        return xSpeed;
+    }
+
+    public double getySpeed() {
+        return ySpeed;
     }
 
     public void draw() {
--- src/com/akadatsky/Board.java
+++ src/com/akadatsky/Board.java
@@ -4,6 +4,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
 
     private final GraphicsContext GC;
@@ -13,6 +16,13 @@ public class Board {
     public Board(GraphicsContext gc) {
         this.GC = gc;
         ball = new Ball(gc);
+
+        List<Ball> ballList = new ArrayList<>();
+        ballList.add(new Ball(20, 30, 30, 3, 1.5, gc));
+        ballList.add(new Ball(20, 40, 30, 3, 2.5, gc));
+        ballList.add(new Ball(20, 50, 30, 2, 4.5, gc));
+
+
     }
 
     public void keyPressed(KeyCode code) {
